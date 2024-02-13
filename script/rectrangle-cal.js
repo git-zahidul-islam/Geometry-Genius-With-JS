829e092a7c8c5522c8301737de3fb7e0ecbf5699
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-02-13 06:37:53 +0600

    parallelogram added

--- script/rectrangle-cal.js
+++ script/rectrangle-cal.js
@@ -1,3 +1,19 @@
 function calculatorRectangleArea() {
-    console.log('hello')
+    // rectangle width
+    const rectangleWidth = document.getElementById('rectangle-width');
+    const rectangleWidthText = rectangleWidth.value;
+    const width = parseFloat(rectangleWidthText);
+    // console.log(width)
+
+    // rectangle length
+    const rectangleLength = document.getElementById('rectangle-length');
+    const rectangleLengthText = rectangleLength.value;
+    const length = parseFloat(rectangleLengthText);
+
+    // calculate
+    const area = width * length;
+    // console.log(area);
+    // show in the text
+    const rectangleShowAns = document.getElementById('rectangle-show-ans');
+    rectangleShowAns.innerText = area;
 }
